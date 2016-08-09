9bc16483c646f1871d997c9060a6bce2559c1dd2
mathieuboudreau <emb6150@gmail.com>
2016-08-09 14:35:15 -0230

    Add test to verify existence of config.yml file

--- test/io/test_load_config.py
+++ test/io/test_load_config.py
@@ -5,9 +5,17 @@ class LoadConfigTest(unittest.TestCase):
     
     def setUp(self):
         self.nonExistingFileName = '*WRONGF1LENAME.1X5!'
+        self.requiredConfigFile  = 'config.yml'
         
     def tearDown(self):
         pass
     
     def test_that_non_existing_filename_throws_error(self):
         self.assertRaises(FileNotFoundError, load_config, self.nonExistingFileName)
+
+    def test_that_ensures_config_file_exists(self):
+        'If this test fails, config.yml.example should be renamed to config.yml and filled out'
+        try:
+            load_config(self.requiredConfigFile)
+        except FileNotFoundError as e:
+            assert False
