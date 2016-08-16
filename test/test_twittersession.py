58a4e111b220e372f743ccde1bf999b1e3d7dffe
mathieuboudreau <emb6150@gmail.com>
2016-08-16 15:47:20 -0230

    Add decorator to filter socket resource warnings

--- test/test_twittersession.py
+++ test/test_twittersession.py
@@ -1,6 +1,14 @@
+import warnings
 import unittest
 from stafferscraper import twittersession
 from stafferscraper.io.loadconfig import load_config
+
+def ignore_warnings(test_func):
+    def do_test(self, *args, **kwargs):
+        warnings.simplefilter("ignore", ResourceWarning)
+        test_func(self, *args, **kwargs)
+    return do_test
+
 class LoginTwitterTest(unittest.TestCase):
     
     def setUp(self):
@@ -18,18 +26,21 @@ class LoginTwitterTest(unittest.TestCase):
     def tearDown(self):
         pass
     
+    @ignore_warnings
     def test_to_verify_if_bad_key_credentials_throw_exception(self):
         self.assertRaises(twittersession.LoginAuthError, self.twitter.login, self.BAD_KEY, self.BAD_SECRET)
 
+    @ignore_warnings
     def test_that_session_instance_variable_is_none_after_login_error(self):
         try:
             self.twitter.login(self.BAD_KEY, self.BAD_SECRET)
         except twittersession.LoginAuthError as e:
             self.assertIsNone(self.twitter.getSession())
     
+    @ignore_warnings
     def test_to_verify_that_good_key_credentials_dont_throw_exceptions(self):
         self.twitter.login(self.API_KEY, self.API_SECRET)
-        
+
     def test_getuserid_returns_the_value_that_was_set(self):
         self.twitter.setUserID(self.userID)
         self.assertEqual(self.userID, self.twitter.getUserID())
